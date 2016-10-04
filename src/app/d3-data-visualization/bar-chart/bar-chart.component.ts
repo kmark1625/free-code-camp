6a2be8012aba90e627b4761b1e72bf2058fb8088
Kevin Mark <kmark1625@gmail.com>
2016-10-03 20:03:34 -0500

    Add attempt at adding d3

--- src/app/d3-data-visualization/bar-chart/bar-chart.component.ts
+++ src/app/d3-data-visualization/bar-chart/bar-chart.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    OnChanges,
+    AfterViewInit,
+    Input,
+    ElementRef,
+    ViewChild
+} from '@angular/core';
+import { BarChartConfig } from './bar-chart-config';
+// import * as d3 from 'd3';
+import * as Moment from 'moment';
 
 @Component({
+    selector: 'bar-chart-component',
     moduleId: module.id,
     templateUrl: './bar-chart.component.html'
 })
-export class BarChartComponent {
+export class BarChartComponent implements OnInit {
+    @Input() config: Array<BarChartConfig>;
+    @ViewChild('container') element: ElementRef;
+
     constructor() { }
+
+    ngOnInit () {
+        // console.log(d3);
+        // var w = 960, h = 500;
+        // var svg = d3.select('#chart')
+        //     .append('svg')
+        //     .attr('width', w)
+        //     .attr('height', h);
+
+        // var text = svg
+        //     .append('text')
+        //     .text('hello world')
+        //     .attr('y', 50);
+    }
 }
